fad017d9c328285102e2985251debac00e6d7eea
Wenting Zhao <wentingzhao@outlook.com>
2021-05-19 18:01:40 -0400

    Added a script to process depth dataset.

---
+++ examples/roberta/proofwriter-dataset-V2020.12.2/OWA/process_proof_depth.py
@@ -0,0 +1,38 @@
+import json
+import sys
+import os
+
+
+def process_data(f):
+    content = open(f, 'r')
+    result = [json.loads(jline) for jline in content.read().splitlines()]
+    ret_inputs = []
+    ret_labels = []
+    for i, item in enumerate(result):
+        context = item['theory']
+        for ques in item['questions'].keys():
+            q = item['questions'][ques]['question']
+            #if item['questions'][ques]['QDep'] != 3: continue
+            ans = item['questions'][ques]['QDep']
+            if ans > 5: print("oddd", ans)
+            ret_inputs.append(context+' <sep> '+q+' <sep>')
+            ret_labels.append(str(ans))
+    return ret_inputs, ret_labels
+
+def main():
+    directory = sys.argv[1]
+    outdir = sys.argv[2]
+    for split in ['train', 'dev', 'test']:
+        inputs, labels = process_data("%s/meta-%s.jsonl" % (directory, split))
+        if not os.path.isdir(outdir):
+            os.mkdir(outdir)
+        f = open("%s/%s.input0" % (outdir, split), "w")
+        f2 = open("%s/%s.label" % (outdir, split), "w")
+        for text in inputs: f.write(text+'\n')
+        for label in labels: f2.write(label+'\n')
+        f.close()
+        f2.close()
+
+if __name__ == "__main__":
+    main()
+
